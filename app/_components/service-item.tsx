6a5802b5ac4c7919c94d339a69e5689258eb86b7
Judson Ciribelli <judson.ciribelli@discente.ufma.br>
2024-08-12 11:27:40 -0300

    feat: ensure only authenticated user can book

--- app/_components/service-item.tsx
+++ app/_components/service-item.tsx
@@ -121,7 +121,6 @@ const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
       })
       await createBooking({
         serviceId: service.id,
-        userId: (data?.user as any).id,
         date: newDate,
       })
       handleBookingSheetOpenChange()
