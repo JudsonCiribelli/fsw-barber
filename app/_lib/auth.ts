6b91a27b05bcc41a0df1b8446fe6e2cb3e02a454
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-03-14 11:44:05 -0300

    fix: add next auth secret

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -6,6 +6,7 @@ import GoogleProvider from "next-auth/providers/google"
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(db) as Adapter,
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
