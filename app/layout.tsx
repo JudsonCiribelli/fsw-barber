2cbe42847db8affb8434604ffbc14d7bc2f4e71a
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-03-14 11:59:02 -0300

    refactor: removing unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
 import { Toaster } from "sonner"
-import { Card, CardContent } from "./_components/ui/card"
 import Footer from "./_components/footer"
 import AuthProvider from "./api/auth/[...nextauth]/providers/auth"
 
