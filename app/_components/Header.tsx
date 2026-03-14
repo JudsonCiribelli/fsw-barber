b7035877dbe4a72d38229e7b2aa3d4ab51097ff2
Judson Ciribelli <judson.ciribelli17@gmail.com>
2026-03-14 10:38:35 -0300

    style: remove border on header component and footer component

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -8,7 +8,7 @@ import Link from "next/link"
 
 const Header = () => {
   return (
-    <Card>
+    <Card className="rounded-t-none">
       <CardContent className="flex flex-row items-center justify-between p-5">
         <Link href="/">
           <Image alt="FSW Barber" src="/logo.png" height={18} width={120} />
--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -3,7 +3,7 @@ import { Card, CardContent } from "./ui/card"
 const Footer = () => {
   return (
     <footer>
-      <Card>
+      <Card className="rounded-b-none">
         <CardContent className="px-5 py-6">
           <p className="text-sm text-gray-400">
             © 2023 Copyright<span className="font-bold"> FSW Barber</span>
